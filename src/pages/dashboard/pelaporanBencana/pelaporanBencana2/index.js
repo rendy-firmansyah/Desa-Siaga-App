b3798932016740bc6a01c6e945b719e6e64ff8ff
AhmadRizkyFebianto <ahmadrizky13jkl@gmail.com>
2024-05-29 13:20:39 +0700

    docs : tulisBerita

--- src/pages/dashboard/pelaporanBencana/pelaporanBencana2/index.js
+++ src/pages/dashboard/pelaporanBencana/pelaporanBencana2/index.js
@@ -7,6 +7,7 @@ import nookies from "nookies";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import axios from "axios";
+import DataTable from 'datatables.net-dt';
 
 //islogin
 export async function getServerSideProps(ctx) {
@@ -44,6 +45,7 @@ const PelaporanBencana2 = () => {
   const nextPage = () => {
     Router.push("/dashboard/pelaporanBencana/pelaporanBencana2");
   };
+
   return (
     <section className="container-fluid w-full h-full relative">
       <div className="absolute -z-10 inset-0">
@@ -75,7 +77,7 @@ const PelaporanBencana2 = () => {
               </div>
             </div>
             <div className="overflow-x-auto mt-4">
-              <table className="min-w-full divide-y divide-gray-200 border-2 shadow-lg shad">
+              <table className="table1 min-w-full divide-y divide-gray-200 border-2 shadow-lg shad">
                 <thead className="">
                   <tr>
                     <th class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
--- src/pages/dashboard/tulisBerita/index.js
+++ src/pages/dashboard/tulisBerita/index.js
@@ -12,43 +12,53 @@ const DetailBerita = () => {
         <div className="bg-white p-4 rounded-lg shadow">
           <h1 className="text-black text-4xl font-extrabold font-['Inter'] leading-10 text-center">Berita Kebencanaan</h1>
         </div>
-        
+
+      <Link href="/">
         <div className="mt-6 flex justify-center">
           <div className="bg-indigo-900 rounded-lg p-1 text-center w-screen">
             <h2 className="text-white text-2xl font-extrabold font-['Inter'] leading-10">+ Tambah Berita</h2>
           </div>
         </div>
+      </Link>
 
-        <div className="mt-6 grid grid-cols-1 gap-6">
-  {Array(4).fill().map((_, index) => (
-    <div key={index} className="bg-white rounded-3xl shadow p-6">
-      <div className="flex flex-col md:flex-row items-center justify-between">
-        <div className="md:w-8/12">
-          <div className="flex items-center">
-            <img className="w-64 h-52 rounded-2xl" src="https://via.placeholder.com/265x201" alt="News" />
-            <div className="ml-6">
-              <h3 className="text-black text-xl font-bold leading-loose">
-                Banjir setinggi 150cm di Desa Karangsono, Kec. Bangsalsari, 150 orang dievakuasi.
-              </h3>
-              <p className="mt-4 text-black text-sm font-normal leading-tight text-justify">
-                Lorem ipsum dolor sit amet consectetur. Volutpat sapien vitae in convallis quis magna purus dictum. Tristique eget facilisis ac habitasse dapibus libero tempor blandit consectetur. Tellus bibendum neque eu et dignissim bibendum ipsum egestas scelerisque. Ut nulla morbi ac in at pharetra nam mattis tristique. Suspendisse turpis maecenas amet lobortis laoreet natoque. Pretium eget ipsum nullam vitae orci urna quis enim a. Sed dictum curabitur volutpat odio odio morbi ullamcorper ullamcorper euismod. A nec facilisi et leo eleifend id quam tincidunt.
-              </p>
+        <div className="mt-6 grid grid-cols-1 gap-6 flex-col md:flex">
+          {Array(3).fill().map((_, index) => (
+            <div key={index} className="bg-white rounded-3xl shadow p-6">
+              <div className="flex flex-row items-center justify-between">
+                <div className="md:w-8/8 me-12">
+                  <div className="flex items-center">
+                    <img className="w-64 h-52 rounded-2xl" src="https://via.placeholder.com/265x201" alt="News"/>
+                    <div className="ml-6">
+                      <h3 className="text-black text-xl font-bold leading-loose">
+                        Banjir setinggi 150cm di Desa Karangsono, Kec. Bangsalsari, 150 orang dievakuasi.
+                      </h3>
+                      <p className="mt-4 text-black text-sm font-normal leading-tight text-justify">
+                        Lorem ipsum dolor sit amet consectetur. Volutpat sapien vitae in convallis quis magna purus dictum. Tristique eget facilisis ac habitasse dapibus libero tempor blandit consectetur. Tellus bibendum neque eu et dignissim bibendum ipsum egestas scelerisque. Ut nulla morbi ac in at pharetra nam mattis tristique. Suspendisse turpis maecenas amet lobortis laoreet natoque. Pretium eget ipsum nullam vitae orci urna quis enim a. Sed dictum curabitur volutpat odio odio morbi ullamcorper ullamcorper euismod. A nec facilisi et leo eleifend id quam tincidunt.
+                      </p>
+                    </div>
+                  </div>
+                </div>
+                <div className="mt-4 md:mt-0 md:w-8/12 flex space-x-4">
+                  <div className="w-15 h-15 p-2.5 bg-indigo-900 rounded border border-gray-200 flex justify-center items-center">
+                    <div className="w-7 h-7 relative">
+                      <Image src='/edit-3-svgrepo-com.png' width={800} height={500} className="w-full h-full object-cover"/>
+                    </div>
+                  </div>
+                  <div className="w-15 h-15 p-2.5 bg-red-600 rounded border border-gray-200 flex justify-center items-center">
+                    <div className="w-7 h-7 relative">
+                      <Image src='/trash-bin-2-svgrepo-com.png' width={800} height={500} className="w-full h-full object-cover"/>
+                    </div>
+                  </div>
+                  <div className="w-15 h-15 p-2.5 bg-amber-500 rounded border border-gray-200 flex justify-center items-center">
+                    <div className="w-7 h-7 relative">
+                      <Image src='/pin-svgrepo-com.png' width={800} height={500} className="w-full h-full object-cover"/>
+                    </div>
+                  </div>
+                </div>
+              </div>
             </div>
-          </div>
+          ))}
         </div>
-        <div className="mt-4 md:mt-0 md:w-4/12 flex space-x-4 ps-60">
-          <div className="w-20 h-20 bg-amber-500 rounded border border-gray-200 flex justify-center items-center">
-            <div className="w-6 h-6 relative"></div>
-          </div>
-          <div className="w-20 h-20 bg-red-600 rounded border border-gray-200 flex justify-center items-center"></div>
-          <div className="w-20 h-20 p-2.5 bg-indigo-900 rounded border border-gray-200 flex justify-center items-center">
-            <div className="w-7 h-7 relative"></div>
-          </div>
-        </div>
-      </div>
-    </div>
-  ))}
-</div>
 
 
 
