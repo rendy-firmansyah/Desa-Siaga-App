b2b2d8c23f576c62cafe51197025c133540270f7
AhmadRizkyFebianto <ahmadrizky13jkl@gmail.com>
2024-06-10 13:32:47 +0700

    fix : preview img add berita

--- src/pages/dashboard/tulisBerita/addBerita/index.js
+++ src/pages/dashboard/tulisBerita/addBerita/index.js
@@ -10,6 +10,7 @@ export const addBerita = () => {
     Router.back();}
 
   const [image, setImage] = useState("");
+  const [file, setFile] = useState("");
   const [judulBerita, setJudulBerita] = useState("");
   const [deskripsiBerita, setDeskripsiBerita] = useState("");
   const [selectedKec, setSelectedKec] = useState("");
@@ -17,12 +18,7 @@ export const addBerita = () => {
   const [dataKecamatan,setDataKec] = useState([]);
   const [dataDesa,setDataDesa] = useState([]);
 
-  const handleImageChange = (evt) => {
-    const [file] = evt.target.files;
-    if (file) {
-      setImage(URL.createObjectURL(file));
-    }
-  };
+  console.log(file);
 
   useEffect(() => {
     const fetchData = async () => {
@@ -40,6 +36,14 @@ export const addBerita = () => {
     fetchData();
   }, [selectedKec]);
 
+  const handleImageChange = (evt) => {
+    const [file] = evt.target.files;
+    if (file) {
+      setImage(URL.createObjectURL(file));
+      setFile(file);
+    }
+  };
+
   const handleSubmit = async (e) => {
     e.preventDefault();
 
@@ -47,7 +51,7 @@ export const addBerita = () => {
     formData.append('judul', judulBerita);
     formData.append('deskripsi', deskripsiBerita);
     formData.append('desa_id', selectedDesa);
-    formData.append('gambar', image);
+    formData.append('gambar', file);
 
     try {
       const response = await axios.post('/api/berita', formData, {
@@ -136,6 +140,12 @@ export const addBerita = () => {
               <label className="font-semibold text-md text-black">
                 Input Gambar
               </label>
+              {/* <input
+                onChange={(e) => setImage(e.target.files[0])}
+                className="border rounded p-2 mt-1 text-black border-primary-default bg-input-default"
+                type="file"
+                accept="image/*"
+              /> */}
               <input type="file" id="imgInp" onChange={handleImageChange} />
               {image && <img id="blah" src={image} alt="Uploaded Image" />}
             </div>
