c4b13cc5ebfcbb9b83f4fb56638ea419b4ee8903
Yoga838 <212410102031@mail.unej.ac.id>
2024-06-25 00:15:47 +0700

    fix merge conflict

--- src/pages/api/pengkajian/kebijakan-peraturan.js
+++ src/pages/api/pengkajian/kebijakan-peraturan.js
@@ -4,54 +4,13 @@ export default async function peraturanHandler(req, res) {
   if (req.method === "POST") {
     const { a, b, c, d, e, f, g, h, pengkajian_id } = req.body;
 
-<<<<<<< HEAD
     if (!a || !b || !c || !d || !e || !f || !g || !h) {
       return res
         .status(400)
         .json({ message: "data tidak lengkap", status: "error" });
-=======
-        if(!a || !b || !c || !d || !e || !f || !g || !h){
-            return res.status(400).json({message:"data tidak lengkap",status:'error'})
-        }
-
-        try{
-            // Menghitung jumlah true
-            const jumlahTrue = (a ? 1 : 0) +
-            (b ? 1 : 0) +
-            (c ? 1 : 0) +
-            (d ? 1 : 0) +
-            (e ? 1 : 0) +
-            (f ? 1 : 0) +
-            (g ? 1 : 0) +
-            (h ? 1 : 0);
-
-            let hasilPerhitungan = jumlahTrue * 2.33;
-            const peraturan = await prisma.peraturan.create({
-                data:{
-                    a,
-                    b,
-                    c,
-                    d,
-                    e,
-                    f,
-                    g,
-                    h,
-                    total:hasilPerhitungan,
-                    pengkajian_id:parseInt(pengkajian_id)
-                }
-            })
-
-            return res
-            .status(200)
-            .json({message:"Berhasil Menambahkan data Kebijakan Peraturan",status:'success'})
-        }
-        catch(error){
-            return res.status(500).json({message:"server error",status:'error'})
-        }
->>>>>>> 733ec2f4398c3cd642d0ae7be1a916e6f957e69c
     }
 
-    // try{
+    try{
     // Menghitung jumlah true
     const jumlahTrue =
       (a ? 1 : 0) +
@@ -64,10 +23,7 @@ export default async function peraturanHandler(req, res) {
       (h ? 1 : 0);
 
     // Mengkalikan jumlah true dengan 2.33 jika semua true
-    let hasilPerhitungan = 0;
-    if (jumlahTrue === 8) {
-      hasilPerhitungan = jumlahTrue * 2.33;
-    }
+    let hasilPerhitungan = jumlahTrue * 2.33;
     const peraturan = await prisma.peraturan.create({
       data: {
         a: JSON.parse(a),
@@ -87,9 +43,9 @@ export default async function peraturanHandler(req, res) {
       message: "Berhasil Menambahkan data Kebijakan Peraturan",
       status: "success",
     });
-    // }
-    // catch(error){
-    //     return res.status(500).json({message:"server error",status:'error'})
-    // }
+    }
+    catch(error){
+        return res.status(500).json({message:"server error",status:'error'})
+    }
   }
 }
