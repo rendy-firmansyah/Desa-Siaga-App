614e652f095bf11a84c134f445d24bc6fb89def1
Yoga838 <212410102031@mail.unej.ac.id>
2024-06-24 23:10:38 +0700

    fix typo

--- src/pages/api/pengkajian/index.js
+++ src/pages/api/pengkajian/index.js
@@ -2,9 +2,9 @@ import prisma from "../../../../lib/prisma";
 
 export default async function pengkajianHandler(req, res) {
   if (req.method === "POST") {
-    const {jenins_ancaman,riwayat,desa_id} = req.body
+    const {jenis_ancaman,riwayat,desa_id} = req.body
 
-    if(!jenins_ancaman || !riwayat || !desa_id){
+    if(!jenis_ancaman || !riwayat || !desa_id){
         return res.status(400).json({message:"data tidak lengkap",status:'error'})
     }
     try{
