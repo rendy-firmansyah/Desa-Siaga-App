710dd954c50871bb3f8e436e5edb246ee21cf6ab
Yoga838 <212410102031@mail.unej.ac.id>
2024-06-26 23:19:36 +0700

    fixing error in API and add get api total persentase

--- src/pages/api/pengkajian/peringatan-dini.js
+++ src/pages/api/pengkajian/peringatan-dini.js
@@ -4,39 +4,33 @@ export default async function peringatanDIniHandler(req, res) {
   if (req.method === "POST") {
     const { a, b, pengkajian_id } = req.body;
 
-    if (!a || !b || !pengkajian_id) {
-      return res
-        .status(400)
-        .json({ message: "data tidak lengkap", status: "error" });
-    }
-
     try {
       const jumlahTrue = (JSON.parse(a) ? 1 : 0) + (JSON.parse(b) ? 1 : 0);
 
       let hasilPerhitungan = jumlahTrue * 2.33;
 
-      // const find = await prisma.peringatanDini.findFirst({
-      //     where: {
-      //         pengkajian_id: parseInt(pengkajian_id),
-      //     },
-      // });
+      const find = await prisma.peringatanDini.findFirst({
+          where: {
+              pengkajian_id: parseInt(pengkajian_id),
+          },
+      });
 
-      // if (find) {
-      //   const peringatanDini = await prisma.peringatanDini.update({
-      //     where: {
-      //       id: find.id,
-      //     },
-      //     data: {
-      //       a,
-      //       b,
-      //       total: hasilPerhitungan,
-      //     },
-      //   });
+      if (find) {
+        const peringatanDini = await prisma.peringatanDini.update({
+          where: {
+            id: find.id,
+          },
+          data: {
+            a : JSON.parse(a),
+            b : JSON.parse(b),
+            total: hasilPerhitungan,
+          },
+        });
 
-      //   return res
-      //     .status(200)
-      //     .json({ message: "Berhasil Mengupdate data Penguatan Kapasitas", status: "success" });
-      // }
+        return res
+          .status(200)
+          .json({ message: "Berhasil Mengupdate data Penguatan Kapasitas", status: "success" });
+      }
 
       const peringatanDini = await prisma.peringatanDini.create({
         data: {
