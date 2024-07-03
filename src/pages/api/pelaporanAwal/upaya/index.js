cca221161e3858dbd9fb71bae274f65ce5c1415d
Yoga838 <212410102031@mail.unej.ac.id>
2024-07-03 23:16:46 +0700

    fixing error in data statistik and api upaya

--- src/pages/api/pelaporanAwal/upaya/index.js
+++ src/pages/api/pelaporanAwal/upaya/index.js
@@ -23,6 +23,7 @@ export default async function upayaHandler(req, res) {
       buteki,
       lansia,
     } = req.body;
+
     if (
       !upayaPenanggulangan ||
       !pelayananKesehatan ||
@@ -47,86 +48,90 @@ export default async function upayaHandler(req, res) {
       return res.status(400).json({ message: "Data tidak boleh Kosong!" });
     }
 
-    // try {
-    const find = await prisma.upaya.findFirst({
-      where: {
-        pelaporan_id: parseInt(pelaporan_id),
-      },
-    });
-
-    if (find) {
-      const update = await prisma.upaya.update({
+    try {
+      const find = await prisma.upaya.findFirst({
         where: {
-          id: find.id,
-        },
-        data: {
-          upayaPenanggulangan,
-          pelayananKesehatan,
-          pelayananKesehatanReproduksi,
-          pengendalianPenyakit,
-          DVI,
-          pelayananGizi,
-          logisticKesehatan,
-          pelayananJiwa,
-          HambatanPelayananKesehatan,
-          bantuanYangDiperlukanSegera,
-          rencanaTindakLanjut,
-          statusDesa,
-          kelompokRentan: {
-            update: {
-              bayi: parseInt(bayi),
-              bumil: parseInt(bumil),
-              balita: parseInt(balita),
-              cacat: parseInt(cacat),
-              buteki: parseInt(buteki),
-              lansia: parseInt(lansia),
-            },
-          },
-        },
-      });
-
-      return res.status(200).json({
-        message: "Berhasil menambahkan data upaya",
-        status: "success",
-      });
-    } else {
-      const pelaporan = await prisma.upaya.create({
-        data: {
-          upayaPenanggulangan,
-          pelayananKesehatan,
-          pelayananKesehatanReproduksi,
-          pengendalianPenyakit,
-          DVI,
-          pelayananGizi,
-          logisticKesehatan,
-          pelayananJiwa,
-          HambatanPelayananKesehatan,
-          bantuanYangDiperlukanSegera,
-          rencanaTindakLanjut,
-          statusDesa,
           pelaporan_id: parseInt(pelaporan_id),
         },
-      });
-      const kelompokRentan = await prisma.kelompokRentan.create({
-        data: {
-          bayi: parseInt(bayi),
-          bumil: parseInt(bumil),
-          balita: parseInt(balita),
-          cacat: parseInt(cacat),
-          buteki: parseInt(buteki),
-          lansia: parseInt(lansia),
-          upaya_id: parseInt(pelaporan.id),
+        include: {
+          kelompokRentan: true, // Include kelompokRentan to update it later
         },
       });
-      return res.status(200).json({
-        message: "Berhasil menambahkan data upaya",
-        status: "success",
-      });
+
+      if (find) {
+        const update = await prisma.upaya.update({
+          where: {
+            id: find.id,
+          },
+          data: {
+            upayaPenanggulangan,
+            pelayananKesehatan,
+            pelayananKesehatanReproduksi,
+            pengendalianPenyakit,
+            DVI,
+            pelayananGizi,
+            logisticKesehatan,
+            pelayananJiwa,
+            HambatanPelayananKesehatan,
+            bantuanYangDiperlukanSegera,
+            rencanaTindakLanjut,
+            statusDesa,
+          },
+        });
+        const updateKelompok = await prisma.kelompokRentan.update({
+          where: {
+            id: find.kelompokRentan[0].id,
+          },
+          data: {
+            bayi: parseInt(bayi),
+            bumil: parseInt(bumil),
+            balita: parseInt(balita),
+            cacat: parseInt(cacat),
+            buteki: parseInt(buteki),
+            lansia: parseInt(lansia),
+          },
+        });
+
+        return res.status(200).json({
+          message: "Berhasil menambahkan data upaya",
+          status: "success",
+        });
+      } else {
+        const pelaporan = await prisma.upaya.create({
+          data: {
+            upayaPenanggulangan,
+            pelayananKesehatan,
+            pelayananKesehatanReproduksi,
+            pengendalianPenyakit,
+            DVI,
+            pelayananGizi,
+            logisticKesehatan,
+            pelayananJiwa,
+            HambatanPelayananKesehatan,
+            bantuanYangDiperlukanSegera,
+            rencanaTindakLanjut,
+            statusDesa,
+            pelaporan_id: parseInt(pelaporan_id),
+            kelompokRentan: {
+              create: {
+                bayi: parseInt(bayi),
+                bumil: parseInt(bumil),
+                balita: parseInt(balita),
+                cacat: parseInt(cacat),
+                buteki: parseInt(buteki),
+                lansia: parseInt(lansia),
+              },
+            },
+          },
+        });
+
+        return res.status(200).json({
+          message: "Berhasil menambahkan data upaya",
+          status: "success",
+        });
+      }
+    } catch (error) {
+      return res.status(500).json({ message: "Server error!", status: "failed" });
     }
-    // } catch (error) {
-    //   return res
-    //     .status(500)
-    //     .json({ message: "Server error!", status: "failed" });
-    // }
   }
 }
