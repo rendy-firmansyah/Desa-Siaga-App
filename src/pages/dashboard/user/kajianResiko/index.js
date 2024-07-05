43aab040f1ffcc9af38bea7399988740b92fbdd5
shrmanto <shrmantoch@gmail.com>
2024-07-05 23:07:08 +0700

    fix: form kajian resiko

--- src/pages/dashboard/user/kajianResiko/index.js
+++ src/pages/dashboard/user/kajianResiko/index.js
@@ -33,86 +33,114 @@ export async function getServerSideProps(ctx) {
 const KajianResiko = () => {
   const router = useRouter();
 
-  const [selectedKec, setSelectedKec] = useState("");
-  const [selectedDesa, setSelectedDesa] = useState("");
-  const [dataKecamatan, setDataKec] = useState([]);
-  const [dataDesa, setDataDesa] = useState([]);
-
   const kuisioner = () => {
     router.push(
-      `/dashboard/user/kajianResiko/ancamanRentan?id=${encodeURIComponent(
-        selectedDesa
-      )}`
+      `/dashboard/user/kajianResiko/pilihWilayah`
     );
   };
 
-  useEffect(() => {
-    const fetchData = async () => {
-      const response = await axios.get("/api/Kecamatan");
-      setDataKec(response.data);
-    };
-    fetchData();
-  }, []);
-
-  useEffect(() => {
-    const fetchData = async () => {
-      const response = await axios.get(`/api/desa?id=${selectedKec}`);
-      setDataDesa(response.data);
-    };
-    fetchData();
-  }, [selectedKec]);
   return (
     <section className="container-fluid h-screen relative">
       <div className="absolute -z-10 inset-0">
         <Image src={bgDashboard} alt="bg-image" className="h-full" />
       </div>
-      <div className="h-full flex flex-col justify-center items-center xl:mx-[151px] lg:mx-[121px] md:mx-[80px] mx-[20px]">
-        <div className="text-black font-bold xl:text-[32px] lg:text-[32px] md:text-[28px] text-[24px] text-center">
-          Pilih Wilayah Kecamatan dan Desa
-        </div>
-        <div className="w-[700px] bg-white xl:px-[50px] lg:px-[35px] md:px-[25px] px-[15px] xl:py-[50px] lg:py-[35px] md:py-[25px] py-[15px] shadow-lg mt-[35px]">
-          <div className="">
-            <div className="flex flex-col mt-2">
-              <label className="font-semibold text-md text-black text-center">
-                Pilih Kecamatan
-              </label>
-              <select
-                className="border rounded p-2 mt-1 text-black border-primary-default bg-input-default"
-                onChange={(e) => setSelectedKec(e.target.value)}
-              >
-                <option>Pilih......</option>
-                {dataKecamatan.map((kec) => (
-                  <option value={kec.id}>{kec.nama}</option>
-                ))}
-              </select>
-            </div>
-            <div className="flex flex-col mt-2">
-              <label className="font-semibold text-md text-black text-center">
-                Pilih Desa
-              </label>
-              <select
-                disabled={!selectedKec ? "disabled" : ""}
-                className="border rounded p-2 mt-1 text-black border-primary-default bg-input-default"
-                onChange={(e) => setSelectedDesa(e.target.value)}
-              >
-                <option value="Pilih......">Pilih......</option>
-                {dataDesa
-                  ? dataDesa.map((desa) => (
-                      <option value={desa.id}>{desa.nama}</option>
-                    ))
-                  : null}
-              </select>
-            </div>
+      <div className="flex flex-col justify-center mx-8 md:mx-14 lg:mx-32 xl:mx-32 pt-10">
+        <div className="flex items-start justify-between mb-4">
+          <h1 className="text-black text-xl font-bold">
+            Data Kajian Awal Resiko
+          </h1>
+          <div>
+            <button
+              onClick={kuisioner}
+              type=""
+              className="bg-secondary-default px-4 py-2 hover:bg-secondary-dark transition-all duration-150 rounded-md"
+            >
+              Isi Kuisioner
+            </button>
           </div>
         </div>
-        <div className="my-5 w-[700px]">
-          <button
-            type=""
-            onClick={kuisioner}
-            className="bg-secondary-default w-full py-2 hover:bg-secondary-dark transition-all duration-150 rounded-md"
-          >
-            Lanjut Kuisioner
-          </button>
+        <div className="overflow-x-auto">
+            <table className="min-w-full divide-y divide-gray-200 border-2 shadow-lg shad">
+                <thead className="">
+                <tr>
+                    <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                    No
+                    </th>
+                    <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                    Pengisi
+                    </th>
+                    <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                    Nama Kecamatan
+                    </th>
+                    <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                    Nama Desa
+                    </th>
+                    <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                    Tanggal Pengisian
+                    </th>
+                    <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                    Action
+                    </th>
+                </tr>
+                </thead>
+                <tbody class="bg-white divide-y divide-gray-200">
+                    <tr>
+                        <td className="text-black px-6 py-4 whitespace-nowrap">
+                            1
+                        </td>
+                        <td className="text-black px-6 py-4 whitespace-nowrap">
+                            Fulan
+                        </td>
+                        <td className="text-black px-6 py-4 whitespace-nowrap">
+                            Kecamatan A
+                        </td>
+                        <td className="text-black px-6 py-4 whitespace-nowrap">
+                            Desa A1
+                        </td>
+                        <td className="text-black px-6 py-4 whitespace-nowrap">
+                            25 Juni 2024
+                        </td>
+                        <td className="px-6 py-4 whitespace-nowrap flex items-center">
+                            <button class="px-3 py-3 bg-gray-300 rounded-md hover:bg-gray-200 focus:outline-none focus:shadow-outline-blue transition duration-150 ease-in-out">
+                                <svg
+                                width="20px"
+                                height="20px"
+                                viewBox="0 0 24 24"
+                                fill="none"
+                                xmlns="http://www.w3.org/2000/svg"
+                                >
+                                <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+                                <g
+                                    id="SVGRepo_tracerCarrier"
+                                    stroke-linecap="round"
+                                    stroke-linejoin="round"
+                                ></g>
+                                <g id="SVGRepo_iconCarrier">
+                                    {" "}
+                                    <path
+                                    d="M10 10C10 10.5523 10.4477 11 11 11V17C10.4477 17 10 17.4477 10 18C10 18.5523 10.4477 19 11 19H13C13.5523 19 14 18.5523 14 18C14 17.4477 13.5523 17 13 17V9H11C10.4477 9 10 9.44772 10 10Z"
+                                    fill="#0F0F0F"
+                                    ></path>{" "}
+                                    <path
+                                    d="M12 8C12.8284 8 13.5 7.32843 13.5 6.5C13.5 5.67157 12.8284 5 12 5C11.1716 5 10.5 5.67157 10.5 6.5C10.5 7.32843 11.1716 8 12 8Z"
+                                    fill="#0F0F0F"
+                                    ></path>{" "}
+                                    <path
+                                    fill-rule="evenodd"
+                                    clip-rule="evenodd"
+                                    d="M23 4C23 2.34315 21.6569 1 20 1H4C2.34315 1 1 2.34315 1 4V20C1 21.6569 2.34315 23 4 23H20C21.6569 23 23 21.6569 23 20V4ZM21 4C21 3.44772 20.5523 3 20 3H4C3.44772 3 3 3.44772 3 4V20C3 20.5523 3.44772 21 4 21H20C20.5523 21 21 20.5523 21 20V4Z"
+                                    fill="#0F0F0F"
+                                    ></path>{" "}
+                                </g>
+                                </svg>
+                            </button>
+                            <button class="px-3 py-3 ms-2 bg-green-500 rounded-md hover:bg-green-400 focus:outline-none focus:shadow-outline-blue transition duration-150 ease-in-out">
+                                Download
+                            </button>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
         </div>
       </div>
     </section>
