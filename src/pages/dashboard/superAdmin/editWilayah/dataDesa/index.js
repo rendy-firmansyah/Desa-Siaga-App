e22d280267836e047cc38222589e2ef88b90ac75
AhmadRizkyFebianto <ahmadrizky13jkl@gmail.com>
2024-07-06 21:04:45 +0700

    fix : dashboard

--- src/pages/dashboard/superAdmin/editWilayah/dataDesa/index.js
+++ src/pages/dashboard/superAdmin/editWilayah/dataDesa/index.js
@@ -104,6 +104,9 @@ const dataWilayah = () => {
             <thead className="">
               <tr>
                 <th class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                  No
+                </th>
+                <th class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
                   Nama Desa
                 </th>
                 <th class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
