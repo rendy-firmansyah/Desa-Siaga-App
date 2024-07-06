34b98422a57997f446c0f234d8c6f7e2f49f0880
Yoga838 <212410102031@mail.unej.ac.id>
2024-07-06 21:27:06 +0700

    fix api berita

---
+++ src/pages/api/berita/get.js
@@ -0,0 +1,41 @@
+import prisma from "../../../../lib/prisma";
+
+export const config = {
+    api: {
+        bodyParser: false
+    }
+}
+
+export default async function BeritaHandler(req, res) {
+    if(req.method === 'GET'){
+        const {id} = req.query.id;
+        if (id){
+            const bertia = await prisma.berita.findMany({
+                orderBy: {
+                  created_at: 'desc',
+                },
+                where:{
+                    desa_id: parseInt(id)
+                },
+                include:{
+                    desa: true
+                }
+            })
+            return res.status(200).json(berita);
+        }
+        try {
+            const berita = await prisma.berita.findMany({
+                orderBy: {
+                  created_at: 'desc',
+                },
+                include: {
+                  desa: true,
+                },
+              });
+            return res.status(200).json(berita);
+        } catch (error) {
+            console.error("Error during DB operation:", error);
+            return res.status(500).json({ message: "Server error!", status: 'failed' });
+        }
+    }
+}
